b0c7ce8f49d7b4def7a0830e3c4a7ac258a9d374
RaniHirave <36604494+RaniHirave@users.noreply.github.com>
2020-01-27 10:57:20 +0530

    Update nat.tf

--- nat.tf
+++ nat.tf
@@ -1,21 +1,21 @@
-resource "google_compute_router" "devops-practice-router-01" {
-  name    = "devops-practice-router-01"
-  region  = google_compute_subnetwork.devops-practice-subnet-01.region
-  network = "${google_compute_network.devops-practice-vpc-01.name}"
+resource "google_compute_router" "test-router-01" {
+  name    = "test-router-01"
+  region  = google_compute_subnetwork.test-subnet-01.region
+  network = "${google_compute_network.test-vpc-01.name}"
 
   bgp {
     asn = 64514
   }
 }
 
-resource "google_compute_router_nat" "devops-practice-nat-01" {
-  name                               = "devops-practice-nat-01"
-  router                             = google_compute_router.devops-practice-router-01.name
+resource "google_compute_router_nat" "test-nat-01" {
+  name                               = "test-nat-01"
+  router                             = google_compute_router.test-router-01.name
   region                             = "us-east1"
   nat_ip_allocate_option             = "AUTO_ONLY"
   source_subnetwork_ip_ranges_to_nat = "LIST_OF_SUBNETWORKS"
   subnetwork  {
-                      name = google_compute_subnetwork.devops-practice-subnet-01.self_link
+                      name = google_compute_subnetwork.test-subnet-01.self_link
                       source_ip_ranges_to_nat = ["ALL_IP_RANGES"]
                }
 }
